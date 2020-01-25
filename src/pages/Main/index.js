00db63cfbe43eb35fe766178185633aab8e54f4d
Lucas de Freitas <lucasffm@gmail.com>
2020-01-25 17:13:41 -0300

    List users

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import api from '../../services/api';
+
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
 const Main = () => {
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState('');
+
+  const handleAddUser = async () => {
+    try {
+      const response = await api.get(`/users/${newUser}`);
+      const data = {
+        name: response.data.name,
+        login: response.data.login,
+        bio: response.data.bio,
+        avatar: response.data.avatar_url,
+      };
+      setUsers([...users, data]);
+      setNewUser('');
+      Keyboard.dismiss();
+      console.tron.log(users);
+    } catch (error) {
+      setNewUser('');
+      console.tron.log(error);
+    }
+  };
+
   return (
     <Container>
       <Form>
         <Input
           autoCorrect={false}
           autoCapitalize="none"
+          value={newUser}
           placeholder="Adicionar Usuário"
+          onChangeText={setNewUser}
+          returnKeyType="send"
+          onSubmitEditing={handleAddUser}
         />
-        <SubmitButton>
+        <SubmitButton onPress={handleAddUser}>
           <Icon name="add" size={20} color="#FFF" />
         </SubmitButton>
       </Form>
+      <List
+        data={users}
+        keyExtractor={user => user.login}
+        renderItem={({ item }) => (
+          <User>
+            <Avatar source={{ uri: item.avatar }} />
+            <Name>{item.name}</Name>
+            <Bio>{item.bio || 'Sem bio'}</Bio>
+            <ProfileButton onPress={() => {}}>
+              <ProfileButtonText>Ver Perfil</ProfileButtonText>
+            </ProfileButton>
+          </User>
+        )}
+      />
     </Container>
   );
 };
