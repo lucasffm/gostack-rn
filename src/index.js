17f4c8a5c485f2ff9243442222db59c788d9c9be
Lucas de Freitas <lucasffm@gmail.com>
2020-01-25 15:37:50 -0300

    added reactotron for debug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, StatusBar } from 'react-native';
 
+import './config/ReactotronConfig';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
